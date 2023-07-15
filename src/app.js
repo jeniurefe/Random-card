c6faaf39c453867c456fc24577d90539201fa2c6
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-07-15 02:56:55 +0000

    Inicio

--- src/app.js
+++ src/app.js
@@ -8,4 +8,11 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let body = document.querySelector("body");
+  console.log(body);
+  body.classList.add("bg", "bg-success");
+  let nuevoDiv = document.createElement("Div");
+  let app = document.querySelector("#app");
+  nuevoDiv.classList.add("container", "bg", "bg-light");
 };
